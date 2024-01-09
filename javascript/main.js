f0ec0519df31df57735c0dc9ca81f983186c509d
azizafarooq <azizafarooq2000@gmail.com>
2024-01-09 17:37:36 +0100

    feat: js costruttori condizionali

--- javascript/main.js
+++ javascript/main.js
@@ -9,5 +9,29 @@ console.log(utenteEtà);
 const result = utenteKm * 0.21;
 console.log(result);
 
-prezzoBiglietto.innerText = "Il prezzo del tuo biglietto è " + result;
+if (utenteEtà <= 18) {
+    const discountPerc= 20;
+
+    const discount = result * discountPerc / 100;
+    console.log(discount);
+
+    const finalPrice = result - discount;
+    console.log(finalPrice);
+
+    prezzoBiglietto.innerText = "Il prezzo del tuo biglietto è " + finalPrice;
+} else (utenteEtà >= 65) 
+{
+    const discountPerc= 40;
+
+    const discount = result * discountPerc / 100;
+    console.log(discount);
+
+    const finalPrice = result - discount;
+    console.log(finalPrice);
+
+    prezzoBiglietto.innerText = "Il prezzo del tuo biglietto è " + finalPrice;
+}
+    
+
+
 
